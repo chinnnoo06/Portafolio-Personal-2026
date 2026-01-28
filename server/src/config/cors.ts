e0849f7fc264d58c91d1ab1b15488893430faa38
chinnnoo06 <inda5054@gmail.com>
2026-01-28 08:14:46 -0600

    cors changes

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -14,5 +14,6 @@ export const corsOptions: CorsOptions = {
 
         // Bloquea navegadores no autorizados
         return callback(new Error("Error de CORS"), false);
-    }
+    },
+    credentials: true,
 }
